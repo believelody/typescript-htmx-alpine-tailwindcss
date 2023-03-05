0723959a64993371b44b256d992ad566191baf7e
believelody <believelody@gmail.com>
2023-03-06 00:35:12 +0100

    improved products more feature

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -7,39 +7,47 @@ import express, { Request, NextFunction, Response } from "express";
 
 const router = express.Router();
 
-export const productsTitle = 'Products';
+export const productsTitle = "Products";
 
-router.get('/', httpMiddleware.limitQueryValidator, async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const limit = Number(req.query.limit || queryUtil.limitQueryArray[0]);
-    const count = Number(req.query.count || limit);
-    if (req.query.count && req.ctx?.fromHTMX) {
-      const htmxRes = await productService.findAll(limit, count - limit) as ProductResponse;
-      if (req.session) {
-        req.session.meta = { total: htmxRes.total, limit, count };
-      }
-      res.setHeader('HX-Trigger', 'update-context');
-      return res.render('partials/product/list', { ...req.ctx, meta: { total: htmxRes.total, limit, count }, products: htmxRes.products });
-    }
-    const { products, total } = await productService.findAll(count, 0) as ProductResponse;
-    return res.render('pages/products', { ...req.ctx, products, meta: { total, limit, count }, title: productsTitle });
-  } catch (error) {
-    console.log(`In ${req.originalUrl} route : ${error}`);
-		next(error);
-  }
-});
+router.get(
+	"/",
+	httpMiddleware.limitQueryValidator,
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const limit = Number(req.query.limit || queryUtil.limitQueryArray[0]);
+			const count = Number(req.query.count || limit);
+			const { products, total } = (await productService.findAll(
+				count,
+				0
+			)) as ProductResponse;
+			return res.render("pages/products", {
+				...req.ctx,
+				products,
+				meta: { total, limit, count },
+				title: productsTitle,
+			});
+		} catch (error) {
+			console.log(`In ${req.originalUrl} route : ${error}`);
+			next(error);
+		}
+	}
+);
 
-router.get('/:id', httpMiddleware.numericParamsValidator, async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const id = Number(req.params.id);
-    const product = await productService.findOneById(id) as Product;
-    const prevProductId = id - 1;
-    const nextProductId = id + 1;
-    
-    return res.render('pages/products/id', { 
-      ...req.ctx,
-      product: {
-        ...product, url: {
+router.get(
+	"/:id",
+	httpMiddleware.numericParamsValidator,
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const id = Number(req.params.id);
+			const product = (await productService.findOneById(id)) as Product;
+			const prevProductId = id - 1;
+			const nextProductId = id + 1;
+
+			return res.render("pages/products/id", {
+				...req.ctx,
+				product: {
+					...product,
+					url: {
 						back: urlUtil.retrieveAppropriateBackUrl(
 							req.headers["hx-current-url"] as string,
 							"/products"
@@ -48,12 +56,13 @@ router.get('/:id', httpMiddleware.numericParamsValidator, async (req: Request, r
 						next: nextProductId && `/products/${nextProductId}`,
 					},
 				},
-      title: product.title
-    });
-  } catch (error) {
-    console.log(`In ${req.originalUrl} route : ${error}`);
-		next(error);
-  }
-})
+				title: product.title,
+			});
+		} catch (error) {
+			console.log(`In ${req.originalUrl} route : ${error}`);
+			next(error);
+		}
+	}
+);
 
 export const productsController = router;
