b43315333ce44fdaad6006840f7b4ff18b2f48a6
believelody <believelody@gmail.com>
2023-05-07 16:29:01 +0200

    modified external-list view.

--- src/configs/route/route.config.ts
+++ src/configs/route/route.config.ts
@@ -1,14 +1,6 @@
 export const routeConfig = {
 	menu: [
 		{
-			url: "/about",
-			label: "About",
-		},
-		{
-			url: "/contact",
-			label: "Contact Us",
-		},
-		{
 			url: "/posts-1",
 			label: "Blog 1",
 		},
