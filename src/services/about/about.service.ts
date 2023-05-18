b3702c7236c0d064608b8951f6cdf4d046ef8407
believelody <believelody@gmail.com>
2023-05-19 00:38:42 +0200

    fixed about page cards.

--- src/services/about/about.service.ts
+++ src/services/about/about.service.ts
@@ -2,13 +2,15 @@ import { AboutContent } from "@interfaces/about.interface";
 
 const findAll = async (): Promise<AboutContent[]> => {
   return new Promise((resolve) => {
-    resolve(Array(12).map((_, index) => ({
-      src: `https://dummyimage.com/72${index}x40${index}`,
-      alt: `content ${index + 1}`,
-      title: "Chichen Itza",
-      subtitle: "SUBTITLE",
-      content: "Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche."
-    })));
+    const items = Array.from({ length: 12 }, (_, index) => ({
+			src: `https://dummyimage.com/72${index}x40${index}`,
+			alt: `content ${index + 1}`,
+			title: "Chichen Itza",
+			subtitle: "SUBTITLE",
+			content:
+				"Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.",
+		}));
+    resolve(items);
   });
 }
 
